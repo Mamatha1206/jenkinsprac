f8b4697badaf92a8245a97e248af2e1608760456
Mamatha Eudulakanti <119730722+Mamatha1206@users.noreply.github.com>
2025-05-25 14:36:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello from inside the Docker container!")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Flask App deployed with Jenkins, Docker, and Kubernetes!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
